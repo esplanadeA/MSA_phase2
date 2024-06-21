6cf4e4af35b1d6ec23088086a2add1e9c44ae5f9
esplanadeA <anna.li.dev@gmail.com>
2024-06-21 13:38:37 +1200

    adding the controller to db plants

---
+++ HouseholdPlantsManagement/Controllers/PlantsController.cs
@@ -0,0 +1,103 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HouseholdPlantsManagement.Models;
+
+namespace HouseholdPlantsManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlantsController : ControllerBase
+    {
+        private readonly PlantsContext _context;
+
+        public PlantsController(PlantsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/plants
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Plant>>> GetPlants()
+        {
+            return await _context.Plants.ToListAsync();
+        }
+
+        // GET: api/plants/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Plant>> GetPlant(int id)
+        {
+            var plant = await _context.Plants.FindAsync(id);
+
+            if (plant == null)
+            {
+                return NotFound();
+            }
+
+            return plant;
+        }
+
+        // POST: api/plants
+        [HttpPost]
+        public async Task<ActionResult<Plant>> PostPlant(Plant plant)
+        {
+            _context.Plants.Add(plant);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetPlant), new { id = plant.Id }, plant);
+        }
+
+        // PUT: api/plants/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPlant(int id, Plant plant)
+        {
+            if (id != plant.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(plant).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PlantExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/plants/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePlant(int id)
+        {
+            var plant = await _context.Plants.FindAsync(id);
+            if (plant == null)
+            {
+                return NotFound();
+            }
+
+            _context.Plants.Remove(plant);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PlantExists(int id)
+        {
+            return _context.Plants.Any(e => e.Id == id);
+        }
+    }
+}
